a4723b0a91b13d4f17e0a466de935ee5eda01196
KASSTUSS <kostya2003112015@gmail.com>
2022-11-20 01:38:54 +0300

    add z-index

--- js/login_button.js
+++ js/login_button.js
@@ -3,25 +3,35 @@ const loginButton = document.getElementById('login_button');
 const clickLoginButton = (e) => {
     e.preventDefault();
 
-
+    
+    $('html').css({
+        'overflow': 'hidden'
+    });
     $('.transition-elem').css({
         'top': e.clientY + 'px',
         'left': e.clientX + 'px'
     });
     $('.transition-elem').show();
-    $('.transition-elem').css({'animation': 'transition-elem 0.75s ease-out'});
+    $('.transition-elem').css({
+        'animation': 'transition-elem 0.75s ease-out',
+        'z-index': '999'
+    });
 
     setTimeout(() => {
         $('.transition-bg').show();
         setTimeout(() => {
             $('.loader-container').css({
-                'display': 'flex'
+                'display': 'flex',
+                'z-index': '999'
             });
             setTimeout(() => {
                 $('.loader-container').css({
                     'opacity': '1'
                 });
-            },50);
-        },150);
+                $('html').css({
+                    'overflow': 'hidden'
+                });
+            },25);
+        },25);
     },750);
 }
