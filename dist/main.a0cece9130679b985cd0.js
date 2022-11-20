5c25bd41412190fb3b1aabdb1ea07d7a9d5b8041
KASSTUSS <kostya2003112015@gmail.com>
2022-11-20 15:09:18 +0300

    install webpack step 1

---
+++ dist/main.a0cece9130679b985cd0.js
@@ -0,0 +1,2 @@
+alert(1);
+//# sourceMappingURL=main.a0cece9130679b985cd0.js.map
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const { strict } = require("assert");
+const path = require("path");
+
+const mode = process.env.MODE || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: '[name].[contenthash].js',
+    }
+    
+
+}
