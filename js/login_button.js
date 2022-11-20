b924e2a0908901c3df3f8ce0552b8628f7ee8286
KASSTUSS <kostya2003112015@gmail.com>
2022-11-20 12:27:12 +0300

    new loader - faster

--- js/login_button.js
+++ js/login_button.js
@@ -1,40 +1,38 @@
 const loginButton = document.getElementById('login_button');
 
-const clickLoginButton = (e) => {
-    e.preventDefault();
-
-    
-    $('html').css({
-        'overflow': 'hidden'
-    });
-    $('.transition-elem').css({
-        'top': e.clientY + 'px',
-        'left': e.clientX + 'px'
-    });
-    $('.transition-elem').show();
-    $('.transition-elem').css({
+const Login = () => {
+    $('.transition-bg').show();
+    $('.transition-bg').css({
         'z-index': '999'
     });
     setTimeout(() => {
-        document.getElementsByClassName('transition-elem')[0].className = 'transition-elem transition-elem-show';
+        document.getElementsByClassName('transition-bg')[0].className = 'transition-bg transition-bg-show';
+        $('.loader-container').css({
+            'display': 'flex'
+        });
     }, 1);   
     
-
     setTimeout(() => {
-        $('.transition-bg').show();
-        setTimeout(() => {
-            $('.loader-container').css({
-                'display': 'flex',
-                'z-index': '999'
-            });
-            setTimeout(() => {
-                $('.loader-container').css({
-                    'opacity': '1'
-                });
-                $('html').css({
-                    'overflow': 'hidden'
-                });
-            },25);
-        },25);
-    },500);
+        $('.loader-container').css({
+            'opacity': '1'
+        });
+        $('html').css({
+            'overflow': 'hidden'
+        });
+        $('.main-container').css({
+            'display': 'none'
+        });
+    },350);
 }
+
+loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    Login();
+});
+
+window.addEventListener('keydown', (event) => {
+    if(event.keyCode === 13 && !event.repeat) 
+        Login();
+    else
+        return;
+});
