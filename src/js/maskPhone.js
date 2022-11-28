5938d6b92cd51747ac513b2455ecfb9796a05cd4
KASSTUSS <kostya2003112015@gmail.com>
2022-11-28 15:22:22 +0300

    mask phone - 0

--- src/js/maskPhone.js
+++ src/js/maskPhone.js
@@ -1,90 +1,91 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const phoneInputs = document.querySelectorAll('input[data-tel-input]');
-    const mask = '+375(__) ___-__-__';
+document.addEventListener("DOMContentLoaded", function () {
+    var phoneInputs = document.querySelectorAll('input[data-tel-input]');
 
-    const getInputNumberValue = function(input){ // get input only with number
+    var getInputNumbersValue = function (input) {
+        // Return stripped input value — just numbers
         return input.value.replace(/\D/g, '');
-    };
-
-    const getCursorPosition = function(ctrl) {
-        var CaretPos = 0;
-        if ( document.selection ) {
-            ctrl.focus ();
-            var Sel = document.selection.createRange();
-            Sel.moveStart ('character', -ctrl.value.length);
-            CaretPos = Sel.text.length;
-        } else if ( ctrl.selectionStart || ctrl.selectionStart == '0' ) {
-            CaretPos = ctrl.selectionStart;
-        }
-        return CaretPos;
-    };
-
-    const setCursorPosition = function(input, pos) {
-        setTimeout(function(){
-            input.setSelectionRange(pos, pos);
-        },0);
-    };
-
-    const onPhoneInput = function(e){
-        let input = e.target,
-            inputNumbersValue = getInputNumberValue(input),
-            formattedInputValue = '',
-            cursorPosition = getCursorPosition(input);
-        
-        if(cursorPosition === 19) {
-            return input.value = input.value.slice(0,18);
-        }
-
-        if(inputNumbersValue === '375') {
-            input.value = mask;
-            return setCursorPosition(input, 5);
-        }
-
-        if(input.value[cursorPosition] === '_') {
-            input.value = input.value.slice(0,cursorPosition) + input.value.slice(++cursorPosition);
-            setCursorPosition(input, --cursorPosition);
-
-            if(input.value[cursorPosition] === ')') {
-                setCursorPosition(input, cursorPosition+2);
-            }
-
-            if(input.value[cursorPosition] === '-') {
-                setCursorPosition(input, cursorPosition+1);
-            }
-
-            return;
-        }
-        
-        console.log(cursorPosition+'_'+ getCursorPosition(input));
-        
-    };
-
-    const onPhoneFocus = function(e){
-        let input = e.target;
-        if (!input.value) {
-            input.value = mask;
-            setCursorPosition(input, 5);
-            console.log(getCursorPosition(input))
-        } else {
-            return;
-        }
-        
-    };
-
-    const onPhoneBlur = function(e){
-        let input = e.target;
-        if (input.value === mask) {
-            input.value = '';
-        } else {
-            return;
-        }
-        
-    };
-
-    for ( let i=0; i<phoneInputs.length; i++ ) {
-        phoneInputs[i].addEventListener('focus', onPhoneFocus);
-        phoneInputs[i].addEventListener('blur', onPhoneBlur);
-        phoneInputs[i].addEventListener('input', onPhoneInput);
-
     }
-});
+
+    var onPhonePaste = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input);
+        var pasted = e.clipboardData || window.clipboardData;
+        if (pasted) {
+            var pastedText = pasted.getData('Text');
+            if (/\D/g.test(pastedText)) {
+                // Attempt to paste non-numeric symbol — remove all non-numeric symbols,
+                // formatting will be in onPhoneInput handler
+                input.value = inputNumbersValue;
+                return;
+            }
+        }
+    }
+
+    var onPhoneInput = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            selectionStart = input.selectionStart,
+            formattedInputValue = "";
+
+        if (!inputNumbersValue) {
+            return input.value = "";
+        }
+
+        if (input.value.length != selectionStart) {
+            // Editing in the middle of input, not last symbol
+            if (e.data && /\D/g.test(e.data)) {
+                // Attempt to input non-numeric symbol
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        //+375(44) 558-11-41    - 375 44 558 11 41
+
+        //8(044) 558-11-41      - 80 44 558 11 41
+
+        if (["2", "3", "4", "8"].indexOf(inputNumbersValue[0]) > -1) {
+
+            
+            if ((inputNumbersValue[0] == "8")) {
+                inputNumbersValue = "";
+                formattedInputValue = "8(0" + inputNumbersValue;
+            } else {
+                if(inputNumbersValue.substring(0,3) === "375")
+                    inputNumbersValue = "375" + inputNumbersValue.substring(3);
+                else
+                    inputNumbersValue = "375" + inputNumbersValue;
+
+                formattedInputValue = "+375(" + inputNumbersValue.substring(3);
+
+                if (inputNumbersValue.length > 3) {
+                    formattedInputValue += inputNumbersValue.substring(4,5);
+                }
+                if (inputNumbersValue.length > 5) {
+                    formattedInputValue += ') ' + inputNumbersValue.substring(5,8);
+                }
+                if (inputNumbersValue.length >= 9) {
+                    //formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+                }
+                if (inputNumbersValue.length >= 10) {
+                    //formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+                }
+            }
+        } else {
+            formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+        }
+        input.value = formattedInputValue;
+    }
+    var onPhoneKeyDown = function (e) {
+        // Clear input after remove last symbol
+        var inputValue = e.target.value.replace(/\D/g, '');
+        if (e.keyCode == 8 && inputValue.length == 1) {
+            e.target.value = "";
+        }
+    }
+    for (var phoneInput of phoneInputs) {
+        phoneInput.addEventListener('keydown', onPhoneKeyDown);
+        phoneInput.addEventListener('input', onPhoneInput, false);
+        phoneInput.addEventListener('paste', onPhonePaste, false);
+    }
+})
