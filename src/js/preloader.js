5ffd2f85d7d5df6fec6b8fa3b82867cee9e06661
KASSTUSS <kostya2003112015@gmail.com>
2022-12-01 01:47:36 +0300

    1

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,10 +2,10 @@ const logoContainer = document.getElementsByClassName("logo-container");
 const preloaderBG = document.getElementsByClassName("preloader-bg");
 
 document.addEventListener("DOMContentLoaded", function(){
+    document.style.overflow = "hidden";
     setInterval(function(){
         logoContainer[0].classList = "logo-container";
         preloaderBG[0].classList = "preloader-bg preloader-bg-hidden";
-        document.style.overflow = "hidden";
         setTimeout(function(){
             preloaderBG[0].style.display = "none";
             document.style.overflow = "";
