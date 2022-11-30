ea5544e2223dc9b7f091cfedad984f0f8a20abae
KASSTUSS <kostya2003112015@gmail.com>
2022-12-01 01:49:37 +0300

    2

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,13 +2,13 @@ const logoContainer = document.getElementsByClassName("logo-container");
 const preloaderBG = document.getElementsByClassName("preloader-bg");
 
 document.addEventListener("DOMContentLoaded", function(){
-    document.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
     setInterval(function(){
         logoContainer[0].classList = "logo-container";
         preloaderBG[0].classList = "preloader-bg preloader-bg-hidden";
         setTimeout(function(){
             preloaderBG[0].style.display = "none";
-            document.style.overflow = "";
+            document.body.style.overflow = "";
         },500);
     },1500);
 });
