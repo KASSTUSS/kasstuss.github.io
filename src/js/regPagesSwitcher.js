3a703a1087d7e7cf2ef5361ebd1265ad6fe48d34
KASSTUSS <kostya2003112015@gmail.com>
2022-11-30 23:49:32 +0300

    1

--- src/js/regPagesSwitcher.js
+++ src/js/regPagesSwitcher.js
@@ -11,14 +11,30 @@ document.addEventListener("DOMContentLoaded", function(){
     const editPage = function(newStep){
         isNext = newStep > step;
 
+        if (newStep != pages.length) {
+            pages[step].classList = "reg-form-inputs-container";
+            setTimeout(function(){
+                pages[step].style.display = "none";
+                setTimeout(function(){
+                    pages[newStep].style.display = "block";
+                    pages[newStep].classList = "reg-form-inputs-container reg-form-inputs-container-show";
+                    step = newStep;
+                },0);
+            },200);
+        }
+
         if ( isNext ) {
             if( newStep === 1 ) {
                 prevButton.style.display = "block";
+                nextButton.style.position = "absolute";
                 setTimeout(function(){
                     buttons.style.width = "250px";
                     prevButton.style.width = nextButton.style.width = "115px";
                     prevButton.style.opacity = "1";
                 },0);
+                setTimeout(function(){
+                    nextButton.style.position = "";
+                },15);
             }
             if ( newStep+1 === pages.length ) {
                 nextButton.innerHTML = "ГОТОВО";
@@ -34,30 +50,12 @@ document.addEventListener("DOMContentLoaded", function(){
                 buttons.style.width = "215px";
                 setTimeout(function(){
                     prevButton.style.display = "none";
-                },90);
+                },155);
             } 
             if ( step+1 === pages.length ) {
                 nextButton.innerHTML = "ДАЛЕЕ";
-                
-                isLastPage = false;
             }
         }
-
-        if (!isLastPage) {
-            pages[step].classList = "reg-form-inputs-container";
-            setTimeout(function(){
-                pages[step].style.display = "none";
-                setTimeout(function(){
-                    pages[newStep].style.display = "block";
-                    pages[newStep].classList = "reg-form-inputs-container reg-form-inputs-container-show";
-
-                    step = newStep;
-                },0);
-            },200);
-        }
-
-        
-
     };
 
     nextButton.addEventListener("click", function(e){
